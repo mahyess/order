a0063d918f5319b9eeee5e3bd6dd444a7855ad33
Mahesh Prajapati <zephyrr2722@gmail.com>
2019-04-02 21:49:28 +0545

    .

--- models/Operator.js
+++ models/Operator.js
@@ -3,31 +3,11 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const OperatorSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
   restaurant: {
     type: Schema.Types.ObjectId,
     ref: "restaurants"
   },
-  username: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  mobile: {
-    type: String,
-    required: true
-  },
-  address: {
-    type: String,
-    required: true
-  },
-  avatar: {
+  role: {
     type: String
   }
 });
